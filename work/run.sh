5ec95f5875e9a4cff237901506e87cbbbb6ff175
Junjie Yang <yangjunjie0320@gmail.com>
2023-09-18 11:07:02 -0700

    update

--- work/run.sh
+++ work/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH --qos=debug
-#SBATCH --time=00:30:00
+#SBATCH --partition=debug
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
-#SBATCH --tasks-per-node=64
+#SBATCH --ntasks-per-node=28
 #SBATCH --cpus-per-task=1
-#SBATCH --constraint=haswell
-#SBATCH --mem=118G
+#SBATCH --mem=0
 #SBATCH --job-name=cceph
-#SBATCH --output=/global/cscratch1/sd/yang0320/slurm-%x-%j.log
+#SBATCH --output=/scratch/global/yangjunjie/slurm-%x-%j.log
 
 module purge
 module load PrgEnv-gnu/6.0.10
@@ -28,16 +27,20 @@ echo MKL_NUM_THREADS      = $MKL_NUM_THREADS
 echo OPENBLAS_NUM_THREADS = $OPENBLAS_NUM_THREADS
 echo PYSCF_MAX_MEMORY     = $PYSCF_MAX_MEMORY
 
-export TMPDIR=/global/cscratch1/sd/yang0320/$SLURM_JOB_NAME-$SLURM_JOB_ID/
-export PYSCF_TMPDIR=TMPDIR
-echo TMPDIR=$TMPDIR
+source /home/yangjunjie/intel/oneapi/setvars.sh --force;
+export LD_LIBRARY_PATH=$MKLROOT/lib:$LD_LIBRARY_PATH
+
+export TMPDIR=/scratch/global/yangjunjie/$SLURM_JOB_NAME-$SLURM_JOB_ID/
+export PYSCF_TMPDIR=TMPDIR; echo TMPDIR=$TMPDIR
 mkdir -p $TMPDIR
 
 export LOG_TMPDIR=$SLURM_SUBMIT_DIR/out/$SLURM_JOB_NAME-$SLURM_JOB_ID/
 echo LOG_TMPDIR=$LOG_TMPDIR; mkdir -p $LOG_TMPDIR
-ln -s /global/cscratch1/sd/yang0320/slurm-$SLURM_JOB_NAME-$SLURM_JOB_ID.log $LOG_TMPDIR/slurm.out
+ln -s /scratch/global/yangjunjie/slurm-$SLURM_JOB_NAME-$SLURM_JOB_ID.log $LOG_TMPDIR/slurm.out
 
 cd ../wick-main/; export PYTHONPATH=$PWD; cd -;
+export PYTHONPATH=/home/yangjunjie/packages/pyscf/pyscf-main/:$PYTHONPATH;
+export PYTHONUNBUFFERED=TRUE;
 
 time mpiexec -n $SLURM_NTASKS \
-  python gen-cceqs.py
+python gen-cceqs.py
