3be3187c4fc03d1909a9be6b258176358e3b4172
Junjie Yang <yangjunjie0320@gmail.com>
2023-09-30 17:55:27 -0700

    update

--- src/run.sh
+++ src/run.sh
@@ -6,7 +6,6 @@
 #SBATCH --cpus-per-task=28
 #SBATCH --mem=0
 #SBATCH --job-name=cceph
-#SBATCH --output=/scratch/global/yangjunjie/slurm-%x-%j.log
 
 module purge
 module load gcc/9.2.0
@@ -31,18 +30,6 @@ export TMPDIR=/scratch/global/yangjunjie/$SLURM_JOB_NAME-$SLURM_JOB_ID/
 export PYSCF_TMPDIR=TMPDIR; echo TMPDIR=$TMPDIR
 mkdir -p $TMPDIR
 
-export LOG_TMPDIR=$SLURM_SUBMIT_DIR/out/$SLURM_JOB_NAME-$SLURM_JOB_ID/
-echo LOG_TMPDIR=$LOG_TMPDIR; mkdir -p $LOG_TMPDIR
-ln -s /scratch/global/yangjunjie/slurm-$SLURM_JOB_NAME-$SLURM_JOB_ID.log $LOG_TMPDIR/slurm.out
-
 cd ../wick-main/; export PYTHONPATH=$PWD; cd -;
 export PYTHONPATH=/home/yangjunjie/packages/pyscf/pyscf-main/:$PYTHONPATH;
 export PYTHONUNBUFFERED=TRUE;
-
-# time python gen-cceqs.py 2 1 4 1 > $LOG_TMPDIR/2141.log
-# time python gen-cceqs.py 2 2 4 1 > $LOG_TMPDIR/2241.log
-# time python gen-cceqs.py 1 1 4 0 > $LOG_TMPDIR/1140.log
-# time python gen-cceqs.py 1 2 4 0 > $LOG_TMPDIR/1240.log
-# time python gen-cceqs.py 1 4 3 0 > $LOG_TMPDIR/1430.log
-time python gen-cceqs.py 1 6 3 0 > $LOG_TMPDIR/1630.log
-# time python gen-cceqs.py 1 8 3 0 > $LOG_TMPDIR/1830.log
---
+++ work/submit.sh
@@ -0,0 +1,43 @@
+submit() {
+    local amp_e_order=1
+    local amp_p_order=$1
+    local max_h_order=$2
+
+    echo "amp_e_order=$amp_e_order"
+    echo "amp_p_order=$amp_p_order"
+    echo "max_h_order=$max_h_order"
+
+    local epccdir="./epcc-e${amp_e_order}-p${amp_p_order}-h${max_h_order}/"
+
+    for ie in $(seq 0 $amp_e_order); do
+        for ip in $(seq 0 $amp_p_order); do
+            for ih in $(seq 0 $max_h_order); do
+                local workdir="${epccdir}/e${ie}-p${ip}-h${ih}/"
+
+                if [ -d "$workdir" ]; then
+                    echo "workdir=$workdir exists, will be removed"
+                    rm -rf "$workdir"
+                fi
+
+                mkdir -p "$workdir" || exit 1
+
+                cd "$workdir" || exit 1
+
+                cp ../../../src/main.py .
+                cp ../../../src/run.sh  .
+
+                echo "python main.py --bra_e_order=$ie --bra_p_order=$ip --max_h_order=$ih --amp_p_order=$amp_p_order" >> run.sh
+
+                # Uncomment and modify the following line to submit the job
+                # sbatch --partition=serial --time=120:00:00 --job-name=$method run.sh
+
+                cd - || exit 1
+            done
+        done
+    done
+}
+
+submit 2 3
+submit 4 3
+submit 6 3
+submit 8 3
