3bc31685b43f90bb621c4c4d7dbb161b97523e4c
Junjie Yang <yangjunjie0320@gmail.com>
2023-10-01 21:28:50 -0700

    update

--- work/submit.sh
+++ work/submit.sh
@@ -14,22 +14,28 @@ submit() {
             for ih in $(seq 0 $max_h_order); do
                 local workdir="${epccdir}/e${ie}-p${ip}-h${ih}/"
 
-                if [ -d "$workdir" ]; then
-                    echo "workdir=$workdir exists, will be removed"
-                    rm -rf "$workdir"
-                fi
+                # if [ -d "$workdir" ]; then
+                #     echo "workdir=$workdir exists, will be removed"
+                #     rm -rf "$workdir"
+                # fi
 
-                mkdir -p "$workdir" || exit 1
+                # mkdir -p "$workdir" || exit 1
 
                 cd "$workdir" || exit 1
 
                 cp ../../../src/main.py .
-                cp ../../../src/run.sh  .
+                # cp ../../../src/run.sh  .
 
-                echo "python main.py --bra_e_order=$ie --bra_p_order=$ip --com_h_order=$ih --amp_p_order=$amp_p_order" >> run.sh
+                # echo "python main.py --bra_e_order=$ie --bra_p_order=$ip --com_h_order=$ih --amp_p_order=$amp_p_order" >> run.sh
 
                 # Uncomment and modify the following line to submit the job
-                sbatch --partition=serial --time=120:00:00 --job-name=$workdir run.sh
+                # if the Error message is found in slurm-* file, remove the file, and resubmit the job
+                if grep -q "TypeError" "$file"; then
+                    rm -f "$file"
+                    echo "Deleted: $file"
+
+                    sbatch --partition=debug --time=01:00:00 --job-name=$workdir run.sh
+                fi
 
                 cd - || exit 1
             done
